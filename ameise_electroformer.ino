dcca0c3461712f575cce102bbda242c96f62d29a
Martin Sukale <ameise@iMac.fritz.box>
2021-11-04 21:18:20 +0100

    intial

---
+++ ameise_electroformer.ino
@@ -0,0 +1,20 @@
+/* ameise Elektroforming Controller
+ *  
+ * used to put out 0..10V through PWM and external Converter
+ * to control high current plating PSUs
+ * here MUNK vari puls 0..160A
+ * 
+ * It shall ramp up the current according to the master to be plated
+ * and shall do some more automation and monitoring tasks...
+ * 
+ */
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
