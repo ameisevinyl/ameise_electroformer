69dc5eb113c303092febc15fb5f1bdbf7e3a2cfb
ameisevinyl <ameise@amei.se>
2021-11-16 21:31:27 +0100

    added LCS i2C lib...

--- ameise_electroformer.ino
+++ ameise_electroformer.ino
@@ -8,19 +8,29 @@
  * and shall do some more automation and monitoring tasks...
  * 
  */
+#include <Wire.h> // Wire Bibliothek einbinden
+#include <LiquidCrystal_I2C.h> // Vorher hinzugefügte LiquidCrystal_I2C Bibliothek einbinden
+LiquidCrystal_I2C lcd(0x27, 16, 2); //Hier wird festgelegt um was für einen Display es sich handelt. In diesem Fall eines mit 16 Zeichen in 2 Zeilen und der HEX-Adresse 0x27. Für ein vierzeiliges I2C-LCD verwendet man den Code "LiquidCrystal_I2C lcd(0x27, 20, 4)" 
 
-const int I_PWN_PIN = 1; //this pin connects to the converter module ZDAUTO and from there to the MUNK analog inlet
-const int U_PWN_PIN = 2; //this pin connects to the converter module ZDAUTO and from there to the MUNK analog inlet
+const int I_PWM_PIN = 1; //this pin connects to the converter module ZDAUTO and from there to the MUNK analog inlet
+const int U_PWM_PIN = 2; //this pin connects to the converter module ZDAUTO and from there to the MUNK analog inlet
 
 void setup() {
+
+  lcd.init(); //Im Setup wird der LCD gestartet 
+  lcd.backlight(); //Hintergrundbeleuchtung einschalten (lcd.noBacklight(); schaltet die Beleuchtung aus). 
   
-  pinMode (I_PWM_PIN, output);
-  pinMode (U_PWM_PIN, output);
+  pinMode (I_PWM_PIN, OUTPUT);
+  pinMode (U_PWM_PIN, OUTPUT);
   
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-}Doc
+  lcd.init(); //Im Setup wird der LCD gestartet 
+  lcd.setCursor(0, 0);//Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile. 
+  lcd.print("Funduino GmbH"); 
+  lcd.setCursor(0, 1);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
+  lcd.print("Viel Erfolg!"); 
+  delay(1);
+}
