2e8c425b028c7d42f792a0024d95982bd424b422
Martin Sukale <ameise@ramona-studio.local>
2021-11-17 20:20:26 +0100

    display works on arduino uno

--- ameise_electroformer.ino
+++ ameise_electroformer.ino
@@ -10,7 +10,7 @@
  */
 #include <Wire.h> // Wire Bibliothek einbinden
 #include <LiquidCrystal_I2C.h> // Vorher hinzugefügte LiquidCrystal_I2C Bibliothek einbinden
-LiquidCrystal_I2C lcd(0x27, 16, 2); //Hier wird festgelegt um was für einen Display es sich handelt. In diesem Fall eines mit 16 Zeichen in 2 Zeilen und der HEX-Adresse 0x27. Für ein vierzeiliges I2C-LCD verwendet man den Code "LiquidCrystal_I2C lcd(0x27, 20, 4)" 
+LiquidCrystal_I2C lcd(0x27, 20, 4); //Hier wird festgelegt um was für einen Display es sich handelt. In diesem Fall eines mit 16 Zeichen in 2 Zeilen und der HEX-Adresse 0x27. Für ein vierzeiliges I2C-LCD verwendet man den Code "LiquidCrystal_I2C lcd(0x27, 20, 4)" 
 
 const int I_PWM_PIN = 1; //this pin connects to the converter module ZDAUTO and from there to the MUNK analog inlet
 const int U_PWM_PIN = 2; //this pin connects to the converter module ZDAUTO and from there to the MUNK analog inlet
@@ -22,15 +22,23 @@ void setup() {
   
   pinMode (I_PWM_PIN, OUTPUT);
   pinMode (U_PWM_PIN, OUTPUT);
+
+  splash_screen();
   
+}
+void splash_screen(){
+  lcd.setCursor(0, 0);//Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile. 
+  lcd.print("ameise electroformer"); 
+  lcd.setCursor(0, 1);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
+  lcd.print("starting up..."); 
+  lcd.setCursor(0, 2);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
+  lcd.print("welcome to"); 
+  lcd.setCursor(0, 3);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
+  lcd.print("chemical warfare!"); 
 
 }
 
+
 void loop() {
-  lcd.init(); //Im Setup wird der LCD gestartet 
-  lcd.setCursor(0, 0);//Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile. 
-  lcd.print("Funduino GmbH"); 
-  lcd.setCursor(0, 1);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
-  lcd.print("Viel Erfolg!"); 
-  delay(1);
+  
 }
